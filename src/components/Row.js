2044c1b60771b6d45c1bf6f74488b822f5240da3
Mateo <M.A.Nasse@student.tudelft.nl>
2025-05-21 01:09:29 +0200

    AAAAAAA

---
+++ src/components/Row.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Row({ guess }) {
+
+    if (guess) {
+        return (
+            <div className="row past">
+                {guess.map((l, i) => {
+                    return (
+                        <div key={i} className={l.color}>{l.key}</div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    return (
+        <div className="row">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
